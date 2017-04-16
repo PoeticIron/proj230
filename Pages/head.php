b4ca779b7ad443ab792241c77bf32bd16cc7ba74
PoeticIron <nmestone@gmail.com>
2017-04-17 00:06:27 +1000

    i dunno

--- Pages/head.php
+++ Pages/head.php
@@ -1,6 +1,14 @@
 <?php
-echo '<head>
+echo '
+<head>
 <script type="text/javascript" src="\proj230\JS\scripts.js"></script>
-		<link href="/proj230/CSS/sitewide.css" rel="stylesheet">
-		</head>';
+<link href="/proj230/CSS/sitewide.css" rel="stylesheet">
+</head>';//Standard header, with reference to the javascript file with scripts, and the CSS stylesheet.
+
+echo '
+<body>
+<div class="content">
+<div class="nav">
+<a href="searchPage.html">Search</a><a href="resultsPage.html">Results</a><a href="registrationPage.html">Registration</a><a href="itemPage.html">Item</a>  
+</div>';//Standard Nav Bar, with links to other pages.
 ?>
