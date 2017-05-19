a159af2b59a5272c67e0684395807910b26df699
PoeticIron <nmestone@gmail.com>
2017-05-20 00:10:23 +1000

    :< <:

--- Operations/review.php
+++ Operations/review.php
@@ -1,12 +1,12 @@
 <?php
 function review($parkCode, $score, $username, $review){
-	echo 'bloop';
+	echo $review;
 	include 'database.php';
 	$getIDstmt = $db->prepare("SELECT id FROM items WHERE parkCode = '".$parkCode."'");
 	$getIDstmt->execute();
 	$parkID = $getIDstmt->fetch();
 	$stmt = $db->prepare("INSERT INTO reviews(parkID, reviewer, ReviewText, ReviewScore) VALUES (?, ?, ?, ?)");
-	$stmt->execute(array($parkID, $username, $review, $score));
+	$stmt->execute(array($parkID['id'], $username, $review, $score));
 
 }
 function chckInp($val){
